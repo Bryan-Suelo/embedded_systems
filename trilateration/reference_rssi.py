b41b9738cd6b89eecca51ab5f5891709ef23abae
root <bryanmker.h@gmail.com>
2022-07-06 14:01:29 -0600

    Reference script developed for formula purposes, distance of MBeacon 1 meter

---
+++ trilateration/reference_rssi.py
@@ -0,0 +1,24 @@
+import csv
+import os
+import statistics
+
+#import pandas as pd
+#import numpy
+
+directory = 'results'
+rssi = []
+#files = []
+
+for filename in os.listdir(directory):
+	f = os.path.join(directory, filename)
+	if os.path.isfile(f):
+		#files.append(f)
+		with open(f, newline='') as infh:
+			reader = csv.reader(infh)
+			for row in reader:
+				if row[0] == 'MBeacon':
+					rssi.append(int(row[2]))
+
+#print(len(files))
+print('Total files in folder: ', len(rssi))
+print('Average calculated: ', round(statistics.mean(rssi),2))
