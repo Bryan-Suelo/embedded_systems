83a5a838127dadb47b87f420126eaa3786ad5b6c
root <bryanmker.h@gmail.com>
2022-07-19 17:22:47 -0600

    Get information from test

---
+++ trilateration/information.py
@@ -0,0 +1,37 @@
+import csv
+import os
+import statistics
+
+#import pandas as pd
+import numpy as np
+
+
+
+Dict = {'Beacon3':[], 'Beacon4':[], 'Beacon5':[], 'Beacon6':[], 'Beacon7':[],
+	'Beaco11':[], 'Beaco12':[], 'Beaco13':[], 'Beaco18':[],
+	'Beaco19':[], 'Beaco20':[], 'Beaco17':[], 'MBeacon':[]}
+
+directory = 'results'
+rssi = np.array([])
+files = []
+
+for filename in os.listdir(directory):
+	f = os.path.join(directory, filename)
+	if os.path.isfile(f):
+		files.append(f)
+		with open(f, newline='') as infh:
+			reader = csv.reader(infh)
+			for row in reader:
+				for key in Dict.keys():
+					if row[0] == key:
+						Dict[key].append(int(row[2]))
+
+for key, value in Dict.items():
+	print(key)
+	print('Total files in folder: ', len(value))
+	print('Average calculated: ', round(statistics.mean(value),2))
+	print('Min value: ', min(value))
+	print('Max value: ', max(value))
+	print('Standard deviation: ', round(statistics.pstdev(value),2))
+	print('\n')
+
